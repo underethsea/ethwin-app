6c06738c9b0eb256ee65bdde332361783ee36b8b
underethsea <underethsea@gmail.com>
2022-11-26 09:32:16 -0800

    projected prize wip

--- src/functions/utils.jsx
+++ src/functions/utils.jsx
@@ -10,10 +10,12 @@ export const Separator = (numb) => {
 
 // possible prize still to accrue before award based on tvl, yield, and time remaining
 export const EstimatePrize = (tvl, secondsRemaining, apy) => {
-  secondsRemaining = 86400 * 5 + 1000;
-  let daysRemaining = parseInt(secondsRemaining / 86400);
+  console.log(secondsRemaining,"sec remain")
+  // secondsRemaining = 86400 * 5 + 1000;
+
+  let daysRemaining = parseInt((secondsRemaining+1) / 86400);
   let estimate = tvl * ((apy/100) * (daysRemaining / 365));
-  // console.log("estimated prize ", estimate);
+  console.log("estimated prize ", estimate);
   return estimate;
 }
 
